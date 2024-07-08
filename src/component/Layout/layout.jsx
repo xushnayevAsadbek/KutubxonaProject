3d817fa342062a9bd3f641f936ac0ad636432cce
asadbek755 <xushnaevasadbek@gmail.com>
2024-07-08 17:02:32 +0500

    layout page commit

---
+++ src/component/Layout/layout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom"
+import { Header } from "../../Header"
+import Section from "../section/section"
+
+export const Layout = () => {
+
+
+    return (
+<>
+<Header />
+<Section/>
+<Outlet />
+</>    )
+}
